442ee2bf42edc673bcd95454e37b61e6af543697
s1072768 <91643192+s1072768@users.noreply.github.com>
2023-11-29 21:36:34 +0800

    Update index.py

--- index.py
+++ index.py
@@ -103,6 +103,24 @@ def spider():
         info += "<a href=" + x.find("a").get("href") + ">" + x.text + "</a><br>"
         info += x.find("a").get("href") + "<br><br>"
     return info
+@app.route("/searchQ", methods=["POST","GET"])
+def searchQ():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""
+        db = firestore.client()     
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
