0c81fd18f95312ca05285b0c28b4b80a57b824fa
s1072768 <91643192+s1072768@users.noreply.github.com>
2023-12-31 12:58:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("s1072768mis-firebase-adminsdk-t6iad-3e1e6e12ac.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 
